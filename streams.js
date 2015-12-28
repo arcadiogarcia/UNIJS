d47723b5e28314f5e464a505590f5ebac17ef4f5
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2015-12-28 23:56:52 +0100

    Stream buffers

--- streams.js
+++ streams.js
@@ -1,18 +1,39 @@
 var Stream = function () {
     var handlers = {};
+    var writeStack = [];
+    var end;
     return {
         on: function (action, callback) {
             handlers[action] = callback;
+            switch (action) {
+                case "data":
+                    while (writeStack.length) {
+                        callback(writeStack.pop());
+                    }
+                    break;
+                case "end":
+                    if (typeof end !== "undefined") {
+                        callback(end);
+                    }
+                    break;
+                default:
+                    console.log("Warning: '" + action + "' is an unrecognized stream action, you might have a typo there");
+            }
         },
         write: function (data) {
-            console.log("Writing: "+data);
             if (typeof handlers["data"] === "function") {
                 handlers["data"](data);
+            } else {
+                writeStack.push(data);
             }
         },
         end: function (data) {
             if (typeof handlers["end"] === "function") {
                 handlers["end"](data);
+            } else {
+                if (typeof end !== "undefined") {
+                    end = data;
+                }
             }
         },
         pipe: function (stream) {
